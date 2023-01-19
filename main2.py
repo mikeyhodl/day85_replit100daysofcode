6ee21ff279887bad8763b45e1871811fd8253158
mikey <mike.owino40@yahoo.com>
2023-01-19 12:18:39 +0000

    Initial commit

---
+++ main2.py
@@ -0,0 +1,97 @@
+from flask import Flask, request, redirect, session
+# my_secret = os.environ['sessionKey']
+import os
+
+app = Flask(__name__)
+app.secret_key = os.environ['sessionKey']
+
+@app.route('/')
+def index():
+    page = ""
+    myName = ""
+    if session.get("myName"):
+        myName = session["myName"]
+    page += f"<h1>{myName}</h1>"
+    f = open("form.html", "r")
+    page += f.read()
+    f.close()
+    return page
+
+###### NEW BIT #######################
+@app.route("/setName", methods=["POST"])
+def setName():
+    session["myName"] = request.form["name"]
+    return redirect("/")
+
+@app.route("/reset")
+def reset():
+    session.clear()
+    return redirect("/")
+
+############################################
+app.run(host='0.0.0.0', port=81)
+
+# # from flask import Flask, request, redirect, session
+# # import os
+
+# # app = Flask(__name__)
+# # # app.secret_key = "THIS_IS_THE_SECRET_KEY"
+# # app.secret_key = os.environ['sessionKey']
+
+# # @app.route('/')
+# # def index():
+# #     page = ""
+# #     myName = ""
+# #     if session["myName"]:
+# #         myName = session["myName"]
+# #     page += f"<h1>{myName}</h1>"
+# #     f = open("form.html", "r")
+# #     page += f.read()
+# #     f.close()
+# #     return page
+
+# # @app.route("/setName", methods=["POST"])
+# # def setName():
+# #     session["myName"] = request.form["name"]
+# #     return redirect("/")
+
+# # @app.route("/reset")
+# # def reset():
+# #     session.clear()
+# #     return redirect("/")
+
+# # app.run(host='0.0.0.0', port=81)
+
+# from flask import Flask, request, redirect, session
+# import os
+
+# app = Flask(__name__)
+# app.secret_key = os.environ['sessionKey']
+
+
+# @app.route('/')
+# def index():
+#     page = ""
+#     myName = ""
+#     if session["myName"]:
+#         myName = session["myName"]
+#     page += f"<h1>{myName}</h1>"
+#     f = open("form.html", "r")
+#     page += f.read()
+#     f.close()
+#     return page
+
+
+# @app.route("/setName", methods=["POST"])
+# def setName():
+#     session["myName"] = request.form["name"]
+#     return redirect("/")
+
+
+# @app.route("/reset")
+# def reset():
+#     session.clear()
+#     return redirect("/")
+
+
+# app.run(host='0.0.0.0', port=81)
